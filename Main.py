d75b34dca45f0611379895624ca5f307b9440d49
Jesus <jesusalcantaras01@gmail.com>
2024-09-11 23:33:00 -0600

    Tarea 5 finalizada

---
+++ Main.py
@@ -0,0 +1,20 @@
+
+from circulo import Circulo
+
+
+circulo1 = Circulo(2.5)
+circulo2 = Circulo(1)
+circulo3 = Circulo(25)
+
+
+#Circulo 1
+circulo1.calcular_area()
+circulo1.calcular_perimetro()
+
+#Circulo 2
+circulo2.calcular_area()
+circulo2.calcular_perimetro()
+
+#Circulo 3
+circulo3.calcular_area()
+circulo3.calcular_perimetro()
