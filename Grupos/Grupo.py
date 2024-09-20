60c335f4408cfb05aa38f0092432abe5d531a65e
Jesus <jesusalcantaras01@gmail.com>
2024-09-19 23:30:41 -0600

    Se eliminaron archivos del hospital

---
+++ Grupos/Grupo.py
@@ -0,0 +1,12 @@
+from typing import List
+from Estudiantes.estudiante import Estudiante 
+from Maestros.maestro import maestro
+from Materias.materia import materia
+
+
+class Grupo:
+    id:int 
+    estudiantes: List [Estudiante] = []
+    maestros: List [maestro] = []
+    materias: List [materia] = []
+    tipo: chr
